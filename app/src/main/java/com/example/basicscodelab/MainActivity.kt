4db0a0f3c173f20de65f01c5bc020ea153b957b2
Kessirin296 <builder@Kessirin296s-MacBook-Pro.local>
2022-07-12 21:25:05 +0700

    Change list of name to index of 1000 list

--- app/src/main/java/com/example/basicscodelab/MainActivity.kt
+++ app/src/main/java/com/example/basicscodelab/MainActivity.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -37,12 +39,17 @@ fun MyApp() {
 }
 
 @Composable
-fun Greetings(names: List<String> = listOf("World", "Compose")) {
+fun Greetings(names: List<String> = List( 1000) { "$it" }) {
     // A surface container using the 'background' color from the theme
     Surface(color = MaterialTheme.colors.background) {
-        Column {
-            for (name in names) {
-                Greeting(name)
+//        Column {
+//            for (name in names) {
+//                Greeting(name)
+//            }
+//        }
+        LazyColumn {
+            items(names) { name ->
+                Greeting(name = name)
             }
         }
     }
